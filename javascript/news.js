e3b11843bd8f7e41fcca8326339e46486abb9a2a
alishasedai <alishasedai21@gmail.com>
2025-07-30 18:39:51 +0545

    For loop condition

---
+++ javascript/news.js
@@ -0,0 +1,9 @@
+console.log("Hellow world");
+
+console.log("Code is also running");
+console.log("Code is looking like a wow!!!");
+
+var a = "I am Alisha Sedai";
+for(var i = 1 ; i<=100 ; i++){
+    console.log(a);
+}
