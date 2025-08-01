c258fe284d5a6be01a23cfdfc46bcc4305b0f95e
alishasedai <alishasedai21@gmail.com>
2025-08-01 18:10:20 +0545

    string methods and properties

--- javascript/news.js
+++ javascript/news.js
@@ -9,5 +9,12 @@ for(var i = 1 ; i<=100 ; i++){
 }
 
 let names  = "harry";
+let names1 = " Sedai";
 
 console.log(names.length);
+console.log(names.toLowerCase());
+console.log(names.toUpperCase());
+console.log(names.slice(2,4));
+console.log(names.replace("harry", "Alisha"));
+console.log(names.concat(names1));
+console.log(names1.trim());
