f49acbabe9fc8d3c4362fefe7309d22c91338530
alishasedai <alishasedai21@gmail.com>
2025-08-08 23:56:54 +0545

    Using the double click

--- DOM/event.js
+++ DOM/event.js
@@ -1,5 +1,5 @@
 let button = document.getElementById("btn");
-button.addEventListener("click", ()=>{
+button.addEventListener("dblclick", ()=>{
     // confirm("I was click")
     // alert(" I was click hehehe😁😂")
     document.getElementsByClassName("box")[0].innerHTML = "<b> You are clicked </b> Enjoy your click!!!</b>"
