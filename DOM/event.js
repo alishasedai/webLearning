236d7b5fcf7d3584a9540d527ba929692e8202d7
alishasedai <alishasedai21@gmail.com>
2025-08-08 23:36:59 +0545

    using the query selector

--- DOM/event.js
+++ DOM/event.js
@@ -1,5 +1,6 @@
 let button = document.getElementById("btn");
 button.addEventListener("click", ()=>{
     // confirm("I was click")
-    alert(" I was click hehehe😁😂")
+    // alert(" I was click hehehe😁😂")
+    document.getElementsByClassName("box")[0].innerHTML = "<b> You are clicked </b> Enjoy your click!!!</b>"
 })
