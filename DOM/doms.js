e67579bc8165fd28c1973b884e1a48361411a98c
alishasedai <alishasedai21@gmail.com>
2025-08-16 23:37:40 +0545

    textContent and innerHTML

---
+++ DOM/doms.js
@@ -0,0 +1,12 @@
+let a = document.getElementById("heading");
+a.innerHTML = "<i><b> Italic </i>"
+console.log(a.textContent);
+
+// innerHtml le chai afno text lai jun wala code lekhya xa ho tesmai tya write 
+//gardinxa jastai <i>Italic lekhya xa vaney abo tya italic ma rakdinxa</i>
+
+// ani textcontent le chai afno text kun xa teslai matrai access garxa
+
+
+
+
