4531a04e04a766911128271d4ce5e58ebaecec5f
alishasedai <alishasedai21@gmail.com>
2025-08-17 16:18:11 +0545

    Promises

--- DOM/doms.js
+++ DOM/doms.js
@@ -41,8 +41,30 @@ const loadScript = (src, callback) => {
 
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback)
 
-alert('alisha');
-alert('');
+// alert('alisha');
+
+
+// Promises
+
+console.log("This is promises");
+
+let prom1 = new Promise((resolve, reject) =>{
+    let a = Math.random();
+    if(a<0.5){
+        reject("No random number was not supporting you")
+    }
+   else{
+     setTimeout(()=>{
+        console.log("yes i am done")
+        resolve("Alisha");
+    },3000);
+   }
+})
+prom1.then((a) =>{
+    console.log(a)
+}).catch((err) =>{
+    console.log(err);
+})
 
 
 
