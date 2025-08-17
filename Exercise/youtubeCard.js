738f57786e24a0066af2fd5ec0c9922219920d9a
alishasedai <alishasedai21@gmail.com>
2025-08-18 00:18:40 +0545

    DOM

--- Exercise/youtubeCard.js
+++ Exercise/youtubeCard.js
@@ -1,27 +1,37 @@
-function createCard(title, cName, views, monthsOld, duration, thumbnail){
+// 
 
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
     let viewNum;
-if(views<1000){
-  viewNum = views/1000 + "k";
-}
-  else if(view>1000000){
-     viewNum = views/1000000 + "M";
-  }
-  else{
-    viewNum = views/1000 + "K"
-  }
+    if (views < 1000) {
+        viewNum = views; // direct number
+    } else if (views >= 1000000) {
+        viewNum = Math.floor(views / 1000000) + "M"; // million
+    } else {
+        viewNum = Math.floor(views / 1000) + "K"; // thousand
+    }
 
-    let html = `        <div class="card">
-            <div class="image">
-                <img src=${thumbnail} alt="">
-                <div class="capsule">${duration}</div>
-            </div>
-            <div class="text">
-                <h1>${title}</h1>
-                <p>${cName}. ${viewNum} views . 2 months ago</p>
-            </div>
-        </div>`
+    let html = `
+      <div class="card">
+          <div class="image">
+              <img src="${thumbnail}" alt="">
+              <div class="capsule">${duration}</div>
+          </div>
+          <div class="text">
+              <h1>${title}</h1>
+              <p>${cName} · ${viewNum} views · ${monthsOld} months ago</p>
+          </div>
+      </div>`;
 
-        document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+    document.querySelector(".container").insertAdjacentHTML("beforeend", html);
+
 }
-createCard()
+
+createCard(
+  "Introduction to Backend | Sigma web dev video #3",
+  "Code with Harry",
+  56000,
+  7,
+  "31:22",
+  "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg"
+);
+
