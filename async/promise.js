6f52c33a97e40950bb5dfa4703dc274853cc85fb
alishasedai <alishasedai21@gmail.com>
2025-08-18 23:54:12 +0545

    try catch

--- async/promise.js
+++ async/promise.js
@@ -8,24 +8,30 @@
 // }
 
 
-async function getDAta(){
-   let x =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
-   let data = await x.text()
-     console.log(data)
-    //  return 4500
-    }
-async function main(){
-console.log("Loading modeules");
+// async function getDAta(){
+//    let x =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//    let data = await x.text()
+//      console.log(data)
+//     //  return 4500
+//     }
+// async function main(){
+// console.log("Loading modeules");
 
-console.log("Do something else");
+// console.log("Do something else");
 
-console.log("Load Data");
+// console.log("Load Data");
 
 
-let data = await getDAta();
-console.log(data);
+// let data = await getDAta();
+// console.log(data);
 
-console.log("Processing data");
-console.log("Task 2");
-}
-main();
+// console.log("Processing data");
+// console.log("Task 2");
+// }
+// main();
+
+let a = prompt('Enter a 1st number');
+let b = prompt('Enter a 2nd number ');
+let sum = parseInt(a)+ parseInt(b)
+
+console.log(sum);
