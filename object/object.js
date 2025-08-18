25d82d3cde4f7878c34aa177b544da24a31d876a
alishasedai <alishasedai21@gmail.com>
2025-08-19 01:29:10 +0545

    object and class

--- object/object.js
+++ object/object.js
@@ -1,14 +1,51 @@
-let obj ={
-    a: 1,
-    b: "Harry"
-}
-console.log(obj);
+// let obj ={
+//     a: 1,
+//     b: "Harry"
+// }
+// console.log(obj);
 
-let animal ={
-    eats: true
-};
-let rabbit = {
-    jumps : true
+// let animal ={
+//     eats: true
+// };
+// let rabbit = {
+//     jumps : true
+// }
+
+// rabbit.__proto__ =animal;
+
+class Animal{
+    constructor (name){
+        this.name = name;
+        console.log("Object is created")
+    }
+
+    eats(){
+        console.log("kha ra hoon")
+    }
+      jump(){
+        console.log("kudd ra hoon")
+    }
+
 }
 
-rabbit.__proto__ =animal;
+
+ class Lion extends Animal{
+    constructor(name) {
+        super(name);
+        console.log("Lion object is created")
+
+    }
+
+    roar(){
+        console.log("Lions roars");
+    }
+}
+    let  c = new Animal("arzu");
+    console.log(c)
+    console.log(c.eats());
+    console.log(c.jump())
+    let lion = new Lion("Sinha");
+    console.log(lion.name)
+    console.log(lion.roar())
+    lion.eats();
+    lion.jump()
