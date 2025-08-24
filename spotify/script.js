1a0181e4a577ade03b3c3c369ce85c4bb3e27737
alishasedai <alishasedai21@gmail.com>
2025-08-24 19:26:21 +0545

    Making the play music

--- spotify/script.js
+++ spotify/script.js
@@ -15,7 +15,7 @@ for(let index = 0; index<as.length; index++){
     const element = as[index];
 
     if(element.href.endsWith(".mp3")){
-songs.push(element.href)
+songs.push(element.href.split("/songs/")[1])
     }}
 return songs;
 }
@@ -29,7 +29,11 @@ async function main() {
 
 
     let songs = await getSongs();
-    console.log(songs); // यहाँ songs को actual array देखिन्छ
+    // console.log(songs); // यहाँ songs को actual array देखिन्छ
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for(const song of songs ){
+        songUL.innerHTML += `<li>${song.replaceAll("%20" ," ")}</li>`;
+    }
 
     //play the first songs
     var audio = new Audio(songs[0]);
