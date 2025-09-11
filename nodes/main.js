e0cf02b84f0b714ff0e0dab21160066d1dc576f0
alishasedai <alishasedai21@gmail.com>
2025-09-11 23:20:58 +0545

    node json lean with require and import

--- nodes/main.js
+++ nodes/main.js
@@ -1,18 +1,29 @@
-const http = require('node')
-const fs =  require("fs")
+const http = require("http");
+const fs = require("fs");
 
-// import http from "http";
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1> HELLO WORLD </h1>');
 
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1> HELLO WORLD </h1>");
 });
+
+
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
-})
+  console.log(`Server running at http://${hostname}:${port}`);
+});
+
+
+// Example: using your module
+const al = require("./mymodule2.js");
+console.log(al, __dirname, __filename);
+
+
+
+
 
 
--- nodes/mymodule.js
+++ nodes/mymodule.js
@@ -1,5 +1,14 @@
-export const a = 1;
-export const b = 2;
-export const c = 3;
-export const d = 4;
-export const e = 5;
+// export const a = 1; // named export
+// export const b = 2; // named export
+// export const c = 3; // named export
+// export const d = 4; // named export
+// export const e = 5; // named export
+
+// let obj;
+// export default obj = { //default export
+//     x : 5, 
+//     y: 7
+// }
+
+
+
