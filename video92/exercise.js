1c02a7b983c972bc472d3d950820f6da05b59c86
alishasedai <alishasedai21@gmail.com>
2025-09-13 16:18:00 +0545

    making the folder for different extensions

--- video92/exercise.js
+++ video92/exercise.js
@@ -1,2 +1,24 @@
+import fs from "fs/promises";
+import fsn from "fs";
+// import { exit } from "process";
+import path from "path";
+// console.log(fs)
 
+const basepath = "C:\\Users\\Asus\\Desktop\\SigmaDev\\video92";
+let files = await fs.readdir(basepath);
+// console.log(files);
 
+for (const items of files) {
+  let ext = items.split(".")[items.split(".").length - 1];
+
+  if (ext != "js" && ext != "json" && items.split(".").length > 1) {
+    if (fsn.existsSync(path.join(basepath, ext))) {
+      //move the file to directory
+      fs.rename(path.join(basepath, items), path.join(basepath, ext, items));
+    } else {
+      fs.mkdir(ext);
+      fs.rename(path.join(basepath, items), path.join(basepath, ext, items));
+    }
+  }
+  // console.log(items)
+}
