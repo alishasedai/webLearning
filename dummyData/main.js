cd3342886c65dd4184966cf07fc38d1df725fd19
alishasedai <alishasedai21@gmail.com>
2025-09-14 16:34:19 +0545

    dummy data storing

--- dummyData/main.js
+++ dummyData/main.js
@@ -1,31 +1,39 @@
 
-const express = require('express')
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const employee = require("./models/employees")
+const employee = require("./models/employees");
 
 
-const port = 3000
+// Connect to MongoDB (no .then(), no async/await)
+//mongoose.connect("mongodb://127.0.0.1:27017/company");
+const port = 3000;
 
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index', {foo : 'FOO' })
-})
 
-app.get('/generate', async (req, res) => {
-    //generate random data
-    for(let i = 0; i < 10 ; i++){
-         await employee.create({
-          name : "Alisha",
-          Salary : 40000,
-          language : "php",
-          city : "New York",
-          isManager : true,
-        });
-    }
-    res.send("10 employees has been sent")
-})
- mongoose.connect("mongodb://127.0.0.1:27017/company");
-app.listen(port, () => {
-    console.log(`app listening in port ${port}`)
-})
+app.get('/', (req, res) => {
+  res.render('index', { foo: "FOO" });
+});
+
+app.get('/generate',async (req, res) => {
+  // Insert 10 employees
+  for (let i = 0; i < 10; i++) {
+   await employee.create({
+      name: "Alisha",
+      Salary: 40000,
+      language: "PHP",
+      city: "New York",
+      isManager: true,
+    });
+  }
+  res.send("10 employees have been sent");
+});
+
+
+// Start server immediately
+mongoose.connect("mongodb://127.0.0.1:27017/company").then(() => {
+  console.log("MongoDB connected!");
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+});
+
+
