1ba9ad09f6c9767514f6188783ac4d905cfaca6c
alishasedai <alishasedai21@gmail.com>
2025-09-18 21:38:08 +0545

    created the simple home page using react

--- vite/first-app/src/components/card.jsx
+++ vite/first-app/src/components/card.jsx
@@ -5,6 +5,9 @@ const card = (props) => {
   return (
    
       <div className='card'>
+        <img src="https://media1.thrillophilia.com/filestore/uwpz857lua13qmvub6um2v93dlrm_IMG%20Worlds%20%20of%20Adventure.jpg" alt="" width={273} height={100}
+        style={{border : "4px solid black"}} 
+        />
       <h1>{props.title}</h1>
       <p>{props.description}</p>
     </div>
