515760b7cdd0932da48fb97ce22c985cd05a6aa0
alishasedai <alishasedai21@gmail.com>
2025-09-18 19:00:55 +0545

    from react making nav and adding css to it

---
+++ vite/first-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar.jsx"
+import Footer from "./components/Footer.jsx"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ vite/first-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footerN'>
+      Copyright codeWithAlisha | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
