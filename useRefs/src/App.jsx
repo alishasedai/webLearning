d61026dd8adc7622767387cfab184d55d689d887
alishasedai <alishasedai21@gmail.com>
2025-09-19 21:17:57 +0545

    use the useRef hooks

--- useRefs/src/App.jsx
+++ useRefs/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const a = useRef(0)
   useEffect(() => {
-    console.log("Re-rendering....")
+    a.current = a.current + 1
+    console.log(`Re-rendering a is ${a.current}`)
   
     
   }, )
