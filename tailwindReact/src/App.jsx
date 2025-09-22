63be2ba0ce00082b007011d51f1dcbc09fc0b746
alishasedai <alishasedai21@gmail.com>
2025-09-22 06:00:38 +0545

    done event handler in add button

--- tailwindReact/src/App.jsx
+++ tailwindReact/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Navbar from './Navbar'
 import './App.css'
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
   const [todo, setTodo] = useState("")
@@ -17,8 +18,26 @@ function App() {
   }
 
    const handleAdd = ()=>{
-    setTodo 
+    setTodos([...todos, {id:uuidv4() ,todo, isCompleted : false}])
+    setTodo("")
+    console.log(todos)
+}  
 
+
+const handleChange = (e) =>{
+  setTodo(e.target.value)
+  }
+
+        
+  const handleCheckBox = (e)=>{
+    let id = e.target.name;
+    let index = todos.findIndex(item =>{
+      return item.id === id;
+
+    })
+    let newTodos = [...todos] ;
+    newTodos[index].isCompleted = !newTodos[index].isCompleted
+    setTodos(newTodos)
   }
   return (
     <>
@@ -26,22 +45,26 @@ function App() {
      <div className="container mx-auto my-5 rounded-xl bg-violet-200 p-5 min-h-[80vh]">
       <div className="addTodo">
         <h2 className='text-lg font-bold'>Add a Todo</h2>
-        <input type="text" className='bg-white rounded-xl p-2 w-160' />
+        <input onChange={handleChange} value={todo} type="text" className='bg-white rounded-xl p-2 w-160' />
         <button onClick={handleAdd} className='bg-violet-700 hover:bg-violet-800 p-3 font-bold py-1 text-white rounded-xl mx-6'>Add</button>
       </div>
         <h1 className='text-2xl font-b'>Your Todos</h1>
      <div className="todos">
-      <div className="todo flex">
-        <div className="text">{todo}</div>
+      {todos.map(item=>{
+
+      return <div key={item.id} className="todo flex my-2 justify-between w-1/2">
+        <input name={item.id} onChange={handleCheckBox} type="checkbox" value={item.isCompleted} />
+        <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
         <div className="buttons">
           <button onClick={handleEdit} className='bg-violet-700 hover:bg-violet-800 p-3 font-bold py-1 text-white rounded-xl mx-2'>Edit</button>
           <button onClick={handleDelete} className='bg-violet-700 hover:bg-violet-800 p-3 font-bold py-1 text-white rounded-xl mx-2'>Delete</button>
         </div>
       </div>
+  })}
      </div>
      </div>
     </>
   )
-}
+ }
 
 export default App
