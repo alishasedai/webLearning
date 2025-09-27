8c81eacee0d368dc86e645384fde5629eafa526e
alishasedai <alishasedai21@gmail.com>
2025-09-27 23:56:23 +0545

    we had use here path and element inside of createBrowserElement

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 import {CreateBrowserRouter, Link, RouterProvider} from 'react-router-dom'
 
 function App() {
       const router = CreateBrowserRouter([
-          {},
+          {
+            Path : "/",
+            element : <Home />
+          },
           {}
       ]) 
 
