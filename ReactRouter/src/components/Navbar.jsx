89271816b8ed5dbf57e581dc4c4bfa428aa0a34c
alishasedai <alishasedai21@gmail.com>
2025-09-27 23:53:48 +0545

    accessing the createRouter Element

--- ReactRouter/src/components/Navbar.jsx
+++ ReactRouter/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Navbar = () => {
+
   return (
     <div>
       <nav>
         <a href="/"><li>Home</li></a>
+        <a href="/"><li>About</li></a>
+        <a href="/"><li>Contact Us</li></a>
       </nav>
     </div>
   )
