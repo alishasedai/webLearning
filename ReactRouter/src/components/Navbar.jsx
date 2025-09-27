171aaa1897a09893424c6b5548961d1f61548196
alishasedai <alishasedai21@gmail.com>
2025-09-27 23:46:03 +0545

    practicing react router

---
+++ ReactRouter/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <a href="/"><li>Home</li></a>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
