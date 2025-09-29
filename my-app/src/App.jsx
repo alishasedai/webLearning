8e05489fdd1ce4b58337ec4d4dfd076058fbaa41
alishasedai <alishasedai21@gmail.com>
2025-09-29 21:57:09 +0545

    re rendering

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>I am Alisha Okay</div>
+    </>
+  )
+}
+
+export default App
