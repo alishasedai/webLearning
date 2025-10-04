df2872d135d92a7e9f63c3c41244148696dfb770
alishasedai <alishasedai21@gmail.com>
2025-10-04 15:53:47 +0545

    react expressions

---
+++ render/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    const message = "Hello, welcome to the header component!";
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  )
+}
+
+export default Header
