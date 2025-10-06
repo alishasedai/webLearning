e6cc6729f487b0631bb490eb1353bce09e86df6d
alishasedai <alishasedai21@gmail.com>
2025-10-06 19:31:47 +0545

    alternate elements printings

--- file.js
+++ file.js
@@ -1,6 +1,15 @@
-let a = ["Apple", "Banana", "Cherry"];
-console.log(a)
-a.splice(1,2)
-let b = "Oka"
-console.log(Array.isArray(b))
-console.log(a instanceof Number)
+function alternateCase(str) {
+ let res = [];
+
+for(let i = 0; i<str.length; i+=2){
+    res.push(arr[i]);
+}
+return res;
+}
+
+let arr = [1,2,3,4,5,6,7]
+
+let results = alternateCase(arr);
+console.log(results.join(" "));
+
+
