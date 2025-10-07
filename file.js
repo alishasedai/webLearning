e8b023337a65561fe5396ed841c33748b603cbcd
alishasedai <alishasedai21@gmail.com>
2025-10-07 07:50:01 +0545

    type of Conversions

--- file.js
+++ file.js
@@ -1,14 +1,4 @@
-let arr = [1,2,3,4,5];
-let findArr = 11;
-let flage = false;
-for(let i = 0 ; i<arr.length ; i++){
- if(arr[i] === findArr){
-    console.log("Found at index "+i );
-    flage = true;
-    break;
- }
-
-}
-if(!flage){
-    console.log("Not Found");
-}
+console.log(typeof['Apple','Banana'])
+console.log(typeof(Number('Alisha')))
+let x = Number('Alisha')
+console.log(x)
