3597aa46302bea451220b7bbaeaf9d503942f4be
alishasedai <alishasedai21@gmail.com>
2025-10-08 23:11:35 +0545

    use effect used

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { use } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(2)
+  useEffect(() => {
+    console.log("Component Mounted")
+  }, [])
 
   return (
     <>
     <div>The count is {count}</div>
     <button onClick = {() => {
-      setCount(count + 1)
+      setCount(count **2)
     }}> Update Count</button>
     </>
   )
