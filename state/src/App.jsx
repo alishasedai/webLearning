070c1c676ca03055de4c3840f8bcf8d6540f1992
alishasedai <alishasedai21@gmail.com>
2025-10-09 21:19:42 +0545

    use ref that doesnot render the component

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect} from 'react'
+import { useState , useEffect, useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,18 +8,25 @@ function App() {
   const [count, setCount] = useState(2)
   useEffect(() => {
     alert("Component Mounted")
-  }, [])
+    return () =>{
+      alert("Component Unmounted")
+    }
+  })
    useEffect(() => {
     alert("count Updated ")
   }, [count])
 
+  useEffect(() => {
+    alert("Component Rendered")
+  })
+  const prevCount = useRef()
   return (
-    <>
-    <div>The count is {count}</div>
+    
+    {/* <div>The count is {count}</div>
     <button onClick = {() => {
       setCount(count **2)
-    }}> Update Count</button>
-    </>
+    }}> Update Count</button> */}
+    
   )
 }
 
