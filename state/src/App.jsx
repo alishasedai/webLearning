64e0fe727d637715003f794cd4438100014e8731
alishasedai <alishasedai21@gmail.com>
2025-10-10 14:16:16 +0545

    react router

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -3,29 +3,43 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { use } from 'react'
+import { Routes, Route , Link} from 'react-router'
+import Home from './component/Home'
+import About from './component/About'
+
 
 function App() {
-  const [count, setCount] = useState(2)
-  useEffect(() => {
-    alert("Component Mounted")
-    return () =>{
-      alert("Component Unmounted")
-    }
-  })
-   useEffect(() => {
-    alert("count had Updated ")
-  }, [count])
+  // const [count, setCount] = useState(2)
+  // useEffect(() => {
+  //   alert("Component Mounted")
+  //   return () =>{
+  //     alert("Component Unmounted")
+  //   }
+  // })
+  //  useEffect(() => {
+  //   alert("count had Updated ")
+  // }, [count])
 
-  useEffect(() => {
-    alert("Component Rendered")
-  })
-  const prevCount = useRef( )
+  // useEffect(() => {
+  //   alert("Component Rendered")
+  // })
+  // const prevCount = useRef( )
   return (
+          <>
+          <Link to = "/">Home</Link>
+          <Link to = "/about">About</Link>
+          {/* <Navbar /> */}
+          <Routes>   
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+              </Routes>
+          </>
     
-    {/* <div>The count is {count}</div>
-    <button onClick = {() => {
-      setCount(count **2)
-    }}> Update Count</button> */}
+  //   {/* <div>The count is {count}</div>
+  //   <button onClick = {() => {
+  //     setCount(count **2)
+  //   }}> Update Count</button> */}
+  
     
   )
 }
---
+++ state/src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <a href=""><li>Home</li></a>
+        <a href=""><li>About</li></a>
+        <a href=""><li>Login</li></a>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
