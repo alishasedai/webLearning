c0974e7ea2fbf546a9f6d10ed1d9ed9345b93fbf
alishasedai <alishasedai21@gmail.com>
2025-10-14 12:27:57 +0545

    async

---
+++ jsPractice/async.js
@@ -0,0 +1,17 @@
+function getData(id){
+    return new Promise((resolve, reject) => {
+         setTimeout(() => {
+            console.log(" data " + id);
+            resolve("success");
+         }, 2000)
+    })
+   
+}
+async function getAllData() {
+    await getData(1);
+    await getData(2);
+    await getData(3);
+    await getData(4);
+
+}
+getAllData()
