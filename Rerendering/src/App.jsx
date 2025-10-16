b3e4ddd85ac01de8f6c04f8c8528aec606e8c98e
alishasedai <alishasedai21@gmail.com>
2025-10-16 18:31:45 +0545

    useffect with variables

--- Rerendering/src/App.jsx
+++ Rerendering/src/App.jsx
@@ -1,10 +1,19 @@
 
-import React ,{useState}from 'react'
+import React ,{useState, useEffect}from 'react'
 import { use } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
   const [name, setName] = useState("")
+  const [otherCount, setOtherCount] = useState(5)
+  // useEffect(() => {
+  //   // document.title = `${count} new message`
+
+  // },[])
+  useEffect(() => {
+    document.title = `${otherCount} new message`
+  }, [otherCount])
+
   function update(){
     setCount(count+1)
   }
@@ -13,6 +22,8 @@ const App = () => {
       <input type="text" onChange={(e) => setName(e.target.value)} id="" />
       <h2>{name} has clicked: {count} times!</h2>
       <button onClick={update }>click</button>
+      <h3>Other count : {otherCount}</h3>
+      <button onClick={() => setOtherCount(otherCount+5)}>Increase By 5</button>
     </div>
   )
 }
