dc250d22b709b73bdb2e52604cba717737afb161
alishasedai <alishasedai21@gmail.com>
2025-10-16 17:09:50 +0545

    use state with input times

--- Rerendering/src/App.jsx
+++ Rerendering/src/App.jsx
@@ -1,14 +1,17 @@
 
 import React ,{useState}from 'react'
+import { use } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("")
   function update(){
     setCount(count+1)
   }
   return (
     <div>
-      <h2>click : {count}</h2>
+      <input type="text" onChange={(e) => setName(e.target.value)} id="" />
+      <h2>{name} has clicked: {count} times!</h2>
       <button onClick={update }>click</button>
     </div>
   )
