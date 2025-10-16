168f200274df89375554952e3397c72d43e8b597
alishasedai <alishasedai21@gmail.com>
2025-10-16 09:41:34 +0545

    getting the currency

--- fetch/api.js
+++ fetch/api.js
@@ -3,7 +3,9 @@ const BASE_URL =
 
 
   const dropdowns = document.querySelectorAll(".dropdown select") ;
-
+  const btn = document.querySelector("form button")
+  const fromCurr =document.querySelector(".from select");
+  const toCurr = document.querySelector(".to select");
   for(let select of dropdowns ){
     for (currCode in countryList) {
         let newOPtion = document.createElement("option");
@@ -29,6 +31,18 @@ const BASE_URL =
     let img = element.parentElement.querySelector("img");
     img.src = newSrc
   } 
-  
+
+  btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input")
+    let amtVal =amount.value;
+    if(amtVal === " " || amtVal<1 ){
+        amtVal = 1
+        amount.value = "1"
+    }
+    console.log(fromCurr.value , toCurr.value)
+    // const URL = `${BASE_URL}/${fromCurr}/${toCurr}.json`;
+    
+  })
 
  
