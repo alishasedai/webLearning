670806b9d066abe070cf39acc5165f7adf9802dc
alishasedai <alishasedai21@gmail.com>
2025-10-16 18:36:34 +0545

    clear interval in useEffect

--- Rerendering/src/App.jsx
+++ Rerendering/src/App.jsx
@@ -3,27 +3,39 @@ import React ,{useState, useEffect}from 'react'
 import { use } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("")
-  const [otherCount, setOtherCount] = useState(5)
+  // const [count, setCount] = useState(0)
+  // const [name, setName] = useState("")
+  // const [otherCount, setOtherCount] = useState(5)
+    const [time, setTime] = useState(0)
   // useEffect(() => {
-  //   // document.title = `${count} new message`
+  //   document.title = `${count} new message`
 
   // },[])
-  useEffect(() => {
-    document.title = `${otherCount} new message`
-  }, [otherCount])
+  // useEffect(() => {
+  //   document.title = `${otherCount} new message`
+  // }, [otherCount])
 
-  function update(){
-    setCount(count+1)
-  }
+  // function update(){
+  //   setCount(count+1)
+  // }
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(time+1);
+
+    },1000)
+    return () => {
+      clearInterval(timer)
+    }
+  })
   return (
     <div>
-      <input type="text" onChange={(e) => setName(e.target.value)} id="" />
+      {/* <input type="text" onChange={(e) => setName(e.target.value)} id="" />
       <h2>{name} has clicked: {count} times!</h2>
       <button onClick={update }>click</button>
       <h3>Other count : {otherCount}</h3>
-      <button onClick={() => setOtherCount(otherCount+5)}>Increase By 5</button>
+      <button onClick={() => setOtherCount(otherCount+5)}>Increase By 5</button> */}
+  <h3>Hello</h3>
+        <h3>{time} in seconds</h3>
     </div>
   )
 }
