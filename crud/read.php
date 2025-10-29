ba69fc831f035a9fe34f21a386f7d84b24166564
alishasedai <alishasedai21@gmail.com>
2025-10-29 21:24:39 +0545

    crud read opertion

---
+++ crud/read.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'db.php';
+$sql = "SELECT * FROM student";
+$result = mysqli_query($conn, $sql) or die(" Query Unsucessfull");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reading Data</title>
+</head>
+
+<body>
+    <?php
+    if (mysqli_num_rows($result) > 0) {
+    ?>
+        <table border="1" cellspacing="1" cellpadding="40" align="center">
+
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($row = mysqli_fetch_assoc($result)) {
+
+
+                ?>
+                    <tr>
+                        <td><?php echo $row['id'] ?> </td>
+                        <td><?php echo $row['name'] ?></td>
+                        <td><?php echo $row['address'] ?></td>
+                    </tr>
+            </tbody>
+        <?php } ?>
+        </table>
+    <?php } ?>
+</body>
+
+</html>
