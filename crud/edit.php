2841de04e0d3ece27e104fd0a9e76d786acde1d7
alishasedai <alishasedai21@gmail.com>
2025-10-30 23:59:55 +0545

    edit code

--- crud/edit.php
+++ crud/edit.php
@@ -1,3 +1,27 @@
 <?php
 include "db.php"
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="id">Id</label>
+        <input type="text" name="sid"><br><br>
+        <label for="name">Name</label>
+        <input type="text" name="sname"><br><br>
+        <label for="address">Address</label>
+        <input type="text" name="saddress"><br><br>
+        <button value="submit" type="submit">Submit</button>
+    </form>
+
+</body>
+
+</html>
