dac31dc191eb23b105d8eaed61bf1cda033da684
alishasedai <alishasedai21@gmail.com>
2025-11-02 10:37:10 +0545

    foreach for of, for in , map concept clear

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+// let num =[2,4,5,6,7]
+// let mul =[]
+
+// for(let i =0 ; i<num.length; i++){
+//     // mul[i] = num[i] * 2;
+//     mul.push(num[i]*2);
+// }
+// console.log(mul);
+
+// let foreachs= num.forEach((v) => {
+//     console.log(v*2);
+// })
+
+// let maps = num.map((value) => {
+//     return value*2;
+// })
+// console.log(maps) 
+// function msg(){
+//     let name = document.getElementById("fullname").value;
+//     console.log("Hello",name)
+// }
+
+
+function calc(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    num1 = Number(num1);
+    num2 = Number(num2)
+    console.log(num1 + num2)
+}
+
+
+
+
