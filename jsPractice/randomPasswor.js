f1be17912dd757a6b2b8b5dae60757d579430a24
alishasedai <alishasedai21@gmail.com>
2025-11-29 07:56:48 +0545

    generating the password according to the user lengths

--- jsPractice/randomPasswor.js
+++ jsPractice/randomPasswor.js
@@ -1,26 +1,50 @@
 const userInput = document.getElementsByClassName("userPassword")[0];
+const totalChars = document.getElementById("consistLength");
 const upperLetter = document.getElementById("upperLetter");
-const loewerLetter = document.getElementById("lowerLetter");
+const lowerLetter = document.getElementById("lowerLetter");
 const symbols = document.getElementById("symbol");
 const numbers = document.getElementById("numbers");
+const generate = document.getElementById("generate");
+console.log(totalChars.value);
 
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lower = upper.toLowerCase();
 const nums = "1234567890";
-const symb = "!@#$%(^&*)_+-?><,."
+const symb = "!@#$%(^&*)_+-?><,.";
 
+const getRandom = (data) => {
+  let randomNumber = parseInt(Math.random() * data.length);
+ return data[randomNumber];
+};
 
-const needs = (data) => {
-  let randomNumber = parseInt(Math.random() * data.length );
-  console.log(data[randomNumber])
-}
-needs(symb);
-needs(lower)
+generate.addEventListener("click", () => {
+  generatePassword();
+});
+const generatePassword = (passwords) => {
+    let password = "";
+  if (upperLetter.checked) {
+    password += getRandom(upper);
+   
+  }
+   userInput.value = password;
 
-needs(nums)
+    if (lowerLetter.checked) {
+      password += getRandom(lower);
+    }
+    userInput.value = password;
+    
+    if (symbols.checked) {
+      password += getRandom(symb);
+    }
+    userInput.value = password;
 
-if(upperLetter.checked){
-    needs(upper);
-}
+    
+    if (numbers.checked) {
+      password += getRandom(nums);
+    }
+    userInput.value = password;
 
-
+    if(password.length <= totalChars.value){
+        return generatePassword(password);
+    }
+};
