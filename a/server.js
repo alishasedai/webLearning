d2fc4ffc7dc6b6b487108d91a7f2eb30952c9d44
alishasedai <alishasedai21@gmail.com>
2025-12-14 15:28:42 +0545

    making the .html file and using .get

--- a/server.js
+++ a/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+// app.get('/', (req,res) => {
+//     res.send("Hello world!!");
+// })
 app.get('/', (req,res) => {
-    res.send("Hello world!!");
+    res.sendFile(path.join(__dirname,'public','index.html'))
 })
-
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'))
+})
 app.listen(5000, () => {
     console.log("server starting in port 6000")
 })
